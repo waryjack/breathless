a329df566cace9b6e2fa0387fc6305e1c1279b2e
Joseph <727468+philote@users.noreply.github.com>
2023-04-08 02:13:35 -0400

    implemented SCSS, started cleaning up the sheet

--- module/sheets/BreathlessActorSheet.js
+++ module/sheets/BreathlessActorSheet.js
@@ -35,7 +35,6 @@ export class BreathlessActorSheet extends ActorSheet {
         charData.job = this.actor.system.job;
         charData.healing = this.actor.system.healing;
 
-        // console.log("Chardata name, pronouns, job: ", charData.name, charData.pronouns, charData.job.name);
         // charData.useHealing = (get system setting here)
 
         return charData;
@@ -49,26 +48,7 @@ export class BreathlessActorSheet extends ActorSheet {
 
         if (!this.options.editable) return;
 
-        // Popout editing of items / skills using their individual sheets
-        html.find('.item-create').click(this._onCreateItem.bind(this));
-        html.find('.item-edit').click(this._onEditItem.bind(this));
-        html.find('.item-delete').click(this._onDeleteItem.bind(this));
-
-        // Inline editing
-        html.find('.inline-edit').blur(this._onInlineEdit.bind(this));
-        html.find('.inline-edit-item').blur(this._onInlineEditItem.bind(this));
-        html.find('.toggle-stress').click(this._onToggleStress.bind(this));
-        
-        // on-sheet manipulations
-        html.find('.stepup').click(this._onStepUp.bind(this));
-        html.find('.stepdown').click(this._onStepDown.bind(this));
-
-        // Rolling dice and using items
-        html.find('.roll-dice').click(this._onRollDice.bind(this));
-        html.find('.use-special').click(this._onUseSpecial.bind(this));
-        html.find('.use-healing').click(this._onUseHealing.bind(this));
-        html.find('.catch-breath').click(this._onCatchBreath.bind(this));
-        html.find('.loot-check').click(this._onCheckLoot.bind(this));
+        html.find('.rollable').click(this._onRoll.bind(this));
 
         // Drag/drop support
         let handler = (ev) => this._onDragStart(ev);
@@ -80,36 +60,103 @@ export class BreathlessActorSheet extends ActorSheet {
         });
     }
 
-    _onCreateItem(e) {
-        e.preventDefault();
+    /**
+     * Handle clickable rolls.
+     * @param {Event} event   The originating click event
+     * @private
+     */
+    _onRoll(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        const dataset = element.dataset;
 
-        let el = e.currentTarget;
-        let iType = el.dataset.type;
-        // TODO use the Skill setting, but need a singlular form for this
-        let loc = "breathless.gen.new."+iType;
+        // Handle rolls.
+        if (dataset.rollType) {
+            switch (dataset.rollType) {
+                case 'item-create': {
+                    this._onCreateItem(dataset.type);
+                    return;
+                }
+                case 'item-edit': {
+                    this._onEditItem(element);
+                    return;
+                }
+                case 'item-delete': {
+                    this._onDeleteItem(element);
+                    return;
+                }
+                case 'catch-breath': {
+                    this.actor.catchBreath();
+                    return;
+                }
+                case 'loot-check': {
+                    this._onCheckLoot();
+                    return;
+                }
+                case 'toggle-stress': {
+                    this._onToggleStress(dataset.pos);
+                    return;
+                }
+                case 'stepup': {
+                    this._onStepUp(element);
+                    return;
+                }
+                case 'stepdown': {
+                    this._onStepDown(element);
+                    return;
+                }
+                case 'use-special': {
+                    this.actor.useSpecial();
+                    return;
+                }
+                case 'use-healing': {
+                    this.actor.useHealing();
+                    return;
+                }
+                case 'roll-dice': {
+                    this._onRollDice(element);
+                    return;
+                }
+                default: {
+                    console.error("_onRoll, bad roll type.");
+                    return;
+                }
+            }
+        }
+
+        // Handle rolls that supply the formula directly.
+        if (dataset.roll) {
+            let label = dataset.label ? `[ability] ${dataset.label}` : '';
+            let roll = new Roll(dataset.roll, this.actor.getRollData());
+            roll.toMessage({
+                speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+                flavor: label,
+                rollMode: game.settings.get('core', 'rollMode'),
+            });
+            return roll;
+        }
+    }
+
+    _onCreateItem(iType) {
+        // TODO use the Skill setting, but need a singular form for defaultName
+        let defaultName = "breathless.gen.new."+iType;
 
         let iData = {
-            name: game.i18n.localize(loc),
+            name: game.i18n.localize(defaultName),
             type: iType
         }
 
         return Item.create(iData, {parent:this.actor, renderSheet:false});
     }
 
-    _onEditItem(e) {
-        e.preventDefault();
-
-        let el = e.currentTarget;
+    _onEditItem(el) {
         let id = el.closest(".item").dataset.itemId;
         let item = this.actor.items.get(id);
 
         item.sheet.render(true);
     }
 
-    _onDeleteItem(e) {
-        e.preventDefault();
-
-        let el = e.currentTarget;
+    _onDeleteItem(el) {
         let id = el.closest(".item").dataset.itemId;
 
         let d = new Dialog(
@@ -132,42 +179,13 @@ export class BreathlessActorSheet extends ActorSheet {
                     }
                 },
                 default: "two"
-                // render: html => console.log("Register interactivity in the rendered dialog"),
-                // close: html => console.log("This always is logged no matter which option is chosen")
             }
         );
         d.render(true);
     }
 
-    _onInlineEdit(e) {
-        e.preventDefault();
-        // console.log("fired inline edit method");
 
-        let el = e.currentTarget;
-        let field = el.dataset.field;
-        // console.log("Edited actor field, new value: ", field, el.innerText);
-        return this.actor.update({[field]:el.innerText});
-    }
-
-    _onInlineEditItem(e) {
-        e.preventDefault();
-        // console.log("fired inline edit item method");
-
-        let el = e.currentTarget;
-        let id = el.closest(".item").dataset.itemId;
-        let field = el.dataset.field;
-        let item = this.actor.items.get(id);
-
-        // console.log("Edited item field, new value: ", field, el.innerText);
-        return item.update({[field]:el.innerText});
-    }
-
-
-    _onToggleStress(e) {
-        e.preventDefault();
-
-        let el = e.currentTarget;
-        let pos = el.dataset.pos;
+    _onToggleStress(pos) {
         let currentArray = this.actor.system.stress.states;
         let currentState = currentArray[pos];
         let newState = 0;
@@ -182,50 +200,24 @@ export class BreathlessActorSheet extends ActorSheet {
         return this.actor.update({["system.stress.states"]:currentArray});
     }
 
-    _onRollDice(e) {
-        e.preventDefault();
-        let el = e.currentTarget;
+    _onRollDice(el) {
         let id = el.closest(".item").dataset.itemId;
         return this.actor.rollDice(id);
     }
 
-    _onUseSpecial(e) {
-        e.preventDefault();
-        return this.actor.useSpecial();
-    }
-
-    _onUseHealing(e) {
-        e.preventDefault();
-        return this.actor.useHealing();
-    }
-
-    _onCatchBreath(e) {
-        e.preventDefault();
-        return this.actor.catchBreath();
-    }
-
-    _onStepUp(e) {
-        e.preventDefault();
-        let el = e.currentTarget;
+    _onStepUp(el) {
         let id = el.closest(".item").dataset.itemId;
         let field = el.closest(".item").dataset.field;
-
-        // console.warn("Item id, field for stepup: ", id, field);
         return this.actor.stepUp(id, field);
     }
 
-    _onStepDown(e) {
-        e.preventDefault();
-        let el = e.currentTarget;
+    _onStepDown(el) {
         let id = el.closest(".item").dataset.itemId;
         let field = el.closest(".item").dataset.field;
-
-        // console.warn("Item id, field for stepup: ", id, field);
         return this.actor.stepDown(id, field);
     }
 
-    async _onCheckLoot(e) {
-        e.preventDefault();
+    async _onCheckLoot() {
         const loot = game.tables.filter(table => table.name === "Loot")[0];
         if(loot === undefined) {
             return ui.notifications.error(game.i18n.localize("breathless.error.lootCheck"))
